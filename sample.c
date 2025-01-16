2dc535ecff95776f172434bee44a9fb5fe787b20
kirstin daley <139495495+KirstinDaley@users.noreply.github.com>
2025-01-16 17:06:17 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,19 +1,17 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+ 
+#define BUFSIZE 256
+    
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
-    // Ensure that the user supplied exactly one command-line argument
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <file_path>\n", argv[0]);
-        return EXIT_FAILURE;
+    // Ensure that the user supplied exactly one command line argument
+    if (argc != 2) { 
+        fprintf(stderr, "Please provide the address of a file as an input.\n");
+        return -1;
     }
-
-    // Open the specified file in binary mode
-    FILE* file = fopen(argv[1], "rb");
-    if (!file) {
-        perror("Error opening file");
-        return EXIT_FAILURE;
-    }
-
-    // Seek to the end of the file to
+    char cmd[BUFSIZE] = "wc -c < ";
+    strcat(cmd, argv[1]);
+    system(cmd);
+}
