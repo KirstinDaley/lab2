dfc867b801ab12c56de987c6dda5cb5623d702ff
kirstin daley <139495495+KirstinDaley@users.noreply.github.com>
2025-01-20 14:42:33 -0400

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
     
-int main(int argc, char **argv) {
-    char *items[] = {"boat", "car", "truck", "train"};
+int main(int argc, char **argv) 
+{ char *items[] = {"boat", "car", "truck", "train"};
     int index = atoi(argv[1]);
-    printf("You selected %s\n", items[index]);
-}
+    printf("You selected %s\n", items[index]);}
